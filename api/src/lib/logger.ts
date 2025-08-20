817a9b3e4203f8a293ea6e7500ba2478c6d89d28
QuachNamLuong <quachnamluong@gmail.com>
2025-08-20 01:39:04 +0000

    update

---
+++ api/src/lib/logger.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'http',
+  levels: winston.config.npm.levels,
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join('logs', 'app.log'),
+    }),
+    new winston.transports.File({
+      filename: path.join('logs', 'error.log'), 
+      level: 'error',
+    }),
+  ],
+});
+
+export default logger;
---
+++ api/src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app.js";
+
+app.listen(8080, () => {
+  console.log("API running on http://localhost:8080");
+});
