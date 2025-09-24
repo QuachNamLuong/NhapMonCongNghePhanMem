a51c9fbec1eb6011c4a163cc64c6cd483bfbf8f3
Quach Nam Luong <quachnamluong@gmail.com>
2025-09-24 15:03:13 +0700

    Using Django

--- api/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(8080, () => {
-  console.log("API running on http://localhost:8080");
-});
--- web/src/app/page.js
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <><h1>Hello, your web is work, now edit file /src/app/page.js to see the change</h1></>);
-}
